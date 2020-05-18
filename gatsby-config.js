84b4ada4697220f4b89c9795549c193b424dc15b
Jesse 'A Body' Einfalt <einfaltj88@gmail.com>
2020-05-17 21:34:51 -0600

    Update Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Jesse \'A Body\' Einfalt',
+    author: 'Jesse Einfalt',
+    description: 'My personal site',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
