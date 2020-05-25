9c9a51d25f3bb6bfb9dca1c8dc2d09a1db6f548a
Jesse 'A Body' Einfalt <einfaltj88@gmail.com>
2020-05-25 14:12:05 -0600

    First Pass at adding vertical work experience

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,7 @@ import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
 import continousIntegrationImage from '../images/continous_integration.jpeg'
 import ContinuousIntegration from '../blogs/continuousIntegration'
+import VerticalWorkExperience from './VerticalWorkExperience'
 
 class Main extends React.Component {
   async openBlogArticle() {
@@ -70,21 +71,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Work Experience</h2>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
-          </p>
+          <VerticalWorkExperience/>
           {close}
         </article>
 
---
+++ src/components/icons/WorkIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkIcon = () => (
+  <div className="logo">
+    <span className="icon fa-cog"></span>
+  </div>
+);
+
+export default WorkIcon
