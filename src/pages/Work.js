8068659c522005a83ec96722217e04650b3e64f2
Jesse 'A Body' Einfalt <einfaltj88@gmail.com>
2020-09-23 21:40:06 -0600

    Update Reach and add react-responsive; made Work page mobile friendly

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import VerticalWorkExperience from '../components/VerticalWorkExperience'
+import { useMediaQuery } from 'react-responsive'
 
-const WorkPage = () => (
+const desktopVersion = (
   <>
     <div style={{ paddingBottom: '60px' }}>
       <div style={{ float: 'left' }}>
@@ -22,5 +23,35 @@ const WorkPage = () => (
     <VerticalWorkExperience />
   </>
 )
+
+const mobileVersion = (
+  <>
+    <div style={{ paddingBottom: '30px' }}>
+      <div>
+        <h2 className="major">Work Experience</h2>
+        <a
+          href="https://drive.google.com/file/d/1xgqHlLsALt-eUpqQpUN_7m-lyComdMxi/view?usp=sharing"
+          download="JesseEinfaltResume"
+          target="_blank"
+          className="button"
+          rel="noreferrer"
+        >
+          Download Resume
+        </a>
+      </div>
+    </div>
+    <VerticalWorkExperience />
+  </>
+)
+
+const WorkPage = () => {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 532px)' })
+
+  if (isTabletOrMobile) {
+    return mobileVersion
+  } else {
+    return desktopVersion
+  }
+}
 
 export default WorkPage
